9a2dc962e6201ca1593c883c9830b93473e58a87
sparticvs <sparticvs@popebp.com>
2015-10-05 13:13:24 -0400

    Finish reusable design

--- main.c
+++ main.c
@@ -8,31 +8,74 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#define ALT_DESIGN 1
+
 #ifdef ALT_DESIGN
 
-typedef uint16_t (*init_func)(void **obj);
-typedef uint16_t (*fini_func)(void *obj);
+typedef uint16_t (*init_func)(void);
+typedef void (*fini_func)(void);
 
+/*
+ * When creating a new vector table, you need to add the section to the linker
+ * script. I am not aware of a way to automatically have them added from just
+ * inside the preprocessor.
+ */
+
+// Create a new vector table
+#define NEW_VECTOR_TABLE(DECL) \
+    extern size_t __vector_##DECL##_start, __vector_##DECL##_end; \
+    uint16_t __vector_##DECL##_exec() { \
+        size_t i = 0; \
+        DECL *begin = &__vector_##DECL##_start; \
+        DECL *end = &__vector_##DECL##_end; \
+        printf("Executing Vector Table\n"); \
+        for(i = 0; i < end - begin; i++) { \
+            printf("\tFunction %d @ %08x\n", (int)i, (void*)begin[i]); \
+            (*begin[i])(); \
+        } \
+        return 0; \
+    }
+
+// Create a reference to the vector table
+#define REF_VECTOR_TABLE(DECL) uint16_t __vector_##DECL##_exec()
+
+// Execute the Exec Function for the Vector Table
+#define EXEC_VECTORS(DECL) __vector_##DECL##_exec()
+// Register a vector to a specific table
 #define REGISTER_VECTOR(DECL,ADDR)   \
     static DECL vector_##DECL##_##ADDR \
-    __attribute__((section(".vector.##DECL"))) = ADDR
+    __attribute__((section(".vector." #DECL))) = ADDR
 
+// Example registration macros
 #define REG_INIT(N) REGISTER_VECTOR(init_func, N)
 #define REG_FINI(N) REGISTER_VECTOR(fini_func, N)
 
-uint16_t init_driver_a(void **obj) {
+NEW_VECTOR_TABLE(init_func);
+NEW_VECTOR_TABLE(fini_func);
+
+uint16_t init_driver_a(void) {
     printf("Entered Init Driver A\n");
     return 0;
 }
 REG_INIT(init_driver_a);
 
-uint16_t fini_driver_a(void *obj) {
+void fini_driver_a(void) {
     printf("Entered Init Driver B\n");
-    return 0;
 }
+// End developers only need to have the register macro and typedefs
 REG_FINI(fini_driver_a);
 
-#endif
+int main() {
+    printf("Running Init Functions\n");
+    // You can use these in your system setup, or driver setup functions
+    EXEC_VECTORS(init_func);
+
+    printf("Running Fini Functions\n");
+
+    EXEC_VECTORS(fini_func);
+}
+
+#else
 
 typedef void (*register_func)(void);
 
@@ -72,3 +115,5 @@ int main() {
 
     return 0;
 }
+
+#endif
